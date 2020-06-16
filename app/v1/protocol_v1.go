737cb5f9eec201d206acee7c23c0e9467f4e2a61
bluesky0125 <time2go@yeah.net>
2020-06-16 18:07:37 +0800

    tx size 1,200,000 to 30,000 byte. hrc20 token contract needs 7k while ordinary tx needs below 300

--- app/v1/protocol_v1.go
+++ app/v1/protocol_v1.go
@@ -2,11 +2,13 @@ package v1
 
 import (
 	"fmt"
-	v1Staking "github.com/orientwalt/htdf/app/v1/staking"
 	"os"
 	"sort"
 
+	v1Staking "github.com/orientwalt/htdf/app/v1/staking"
+
 	"github.com/orientwalt/htdf/app/protocol"
+	"github.com/orientwalt/htdf/app/v1/slashing"
 	"github.com/orientwalt/htdf/codec"
 	newevmtypes "github.com/orientwalt/htdf/evm/types"
 	sdk "github.com/orientwalt/htdf/types"
@@ -20,7 +22,6 @@ import (
 	"github.com/orientwalt/htdf/x/mint"
 	"github.com/orientwalt/htdf/x/params"
 	"github.com/orientwalt/htdf/x/service"
-	"github.com/orientwalt/htdf/app/v1/slashing"
 	stake "github.com/orientwalt/htdf/x/staking"
 	"github.com/orientwalt/htdf/x/upgrade"
 	"github.com/sirupsen/logrus"
@@ -31,8 +32,10 @@ import (
 
 const (
 	//
-	RouterKey   = "htdfservice"
-	TxSizeLimit = 1200000 // tx size is limited to 1200000(bytes)
+	RouterKey = "htdfservice"
+	// TxSizeLimit = 1200000 // tx size is limited to 1200000(bytes)
+	// junying-todo, 2020-06-16
+	TxSizeLimit = 30000 // tx size limit shrinked to 35000(bytes) because 4G RAM causes out-of-memory crashing for 500tx-airdrop tx with gaswanted of over 10,000,000 gas
 )
 
 var _ protocol.Protocol = (*ProtocolV1)(nil)
